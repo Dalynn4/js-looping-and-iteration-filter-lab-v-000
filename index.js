d4ce7d196e614e7ddc514a71e823c733736e4903
Randall Robinson <keith_adams007@yahoo.com>
2018-05-20 01:50:08 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 // Code your solution in this file
 function findMatching(array, string) {
- return array.filter(name => name === string);
+ return array.filter(name => name.toUppercase() === string.toUpperCase());
 }
